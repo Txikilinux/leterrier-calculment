e40ac63c6efa05f3d92a96e05b3bfa121db97a3d
Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
2010-10-21 15:02:19 +0200

    bordure autour qgraphicsview à zéro

--- exercice.cpp
+++ exercice.cpp
@@ -116,7 +116,7 @@ void exercice::setImgFond()
 
 void exercice::adapte(QPixmap cheminImage)
 {
-    int bordure=20;
+    int bordure=0;
     QRect ecran;
     ecran=QApplication::desktop()->screenGeometry();
                                                                 //              QPixmap imgFond2 = cheminImage.scaledToHeight(ecran.height()*0.88, Qt::SmoothTransformation);
