a4684b59e4c7d7fe50b9c0ee9c4274948b45492a
Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
2010-10-21 11:58:26 +0200

    Entêtes de licence ajoutées dans les fichiers sources

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
+/**
+  * Implémentation de la fonction main qui permet soit de lancer l'interface générale, soit de lancer un exercice donné, avec éventuellement un niveau donné
+  *
+  * @warning nécessité de respecter la syntaxe
+  * @see https://redmine.ryxeo.com/projects/leterrier-calculment/
+  * @author 2009-2010 Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
+  * @see The GNU Public License (GPL)
+  */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include <QtGui/QApplication>
 #include "interface.h"
 #include "exercice.h"
