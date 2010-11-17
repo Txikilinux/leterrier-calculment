99e3e5a18119e39a78f16dd9604640a83ebee79e
Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
2010-11-17 10:58:10 +0100

    Correction du bug #875

--- abuleduexercicev0.cpp
+++ abuleduexercicev0.cpp
@@ -75,7 +75,7 @@ AbulEduExerciceV0::AbulEduExerciceV0(QWidget *parent) :
 
 AbulEduExerciceV0::~AbulEduExerciceV0()
 {
-    delete this;
+    //delete this;
 }
 
 void AbulEduExerciceV0::setAbeLineLog(QString question, QString answer,
