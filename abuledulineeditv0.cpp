b707aaf1d2c3f8b8ab348834b8d6b9db8055c45e
Philippe Cadaugade <philippe.cadaugade@ryxeo.com>
2012-02-09 12:32:49 +0100

    Mappage touches Pad si verrouillage éteint

--- abuledulineeditv0.cpp
+++ abuledulineeditv0.cpp
@@ -56,6 +56,49 @@ void AbulEduLineEditV0::keyPressEvent(QKeyEvent* event)
         QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_0,Qt::NoModifier,QString(QChar(Qt::Key_0))));
         break;
 
+    // On vérifie si la touche est bien sur le keypad désactivé
+    if (!(event->modifiers() & Qt::KeypadModifier) == 0)
+    {
+    case Qt::Key_Insert:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_0,Qt::NoModifier,QString(QChar(Qt::Key_0))));
+        break;
+
+    case Qt::Key_End:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_1,Qt::NoModifier,QString(QChar(Qt::Key_1))));
+        break;
+
+    case Qt::Key_Down:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_2,Qt::NoModifier,QString(QChar(Qt::Key_2))));
+        break;
+
+    case Qt::Key_PageDown:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_3,Qt::NoModifier,QString(QChar(Qt::Key_3))));
+        break;
+
+    case Qt::Key_Left:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_4,Qt::NoModifier,QString(QChar(Qt::Key_4))));
+        break;
+
+    case Qt::Key_Clear:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_5,Qt::NoModifier,QString(QChar(Qt::Key_5))));
+        break;
+
+    case Qt::Key_Right:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_6,Qt::NoModifier,QString(QChar(Qt::Key_6))));
+        break;
+
+    case Qt::Key_Home:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_7,Qt::NoModifier,QString(QChar(Qt::Key_7))));
+        break;
+
+    case Qt::Key_Up:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_8,Qt::NoModifier,QString(QChar(Qt::Key_8))));
+        break;
+
+    case Qt::Key_PageUp:
+        QApplication::sendEvent(this,new QKeyEvent(QEvent::KeyPress,Qt::Key_9,Qt::NoModifier,QString(QChar(Qt::Key_9))));
+        break;
+    }
     default:
         QLineEdit::keyPressEvent(event);
     }
