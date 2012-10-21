eee28237b46622cbefa049a094a495b883e5594b
Eric Seigne <eric.seigne@ryxeo.com>
2012-10-21 16:21:00 +0200

    adaptation build macos

--- macos/build.sh
+++ macos/build.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 #script de creation de l'image pour OSX
-#Eric Seigne 2011 <eric.seigne@ryxeo.com>
+#Eric Seigne 2011-2012 <eric.seigne@ryxeo.com>
 #documentation https://redmine.ryxeo.com/projects/ryxeo/wiki/Cr%C3%A9er_un_paquet_avec_OSX
 APPNAME="leterrier-calcul-mental"
 APPVERSION="1.0.10"
+VOLNAME="AbulEdu LeTerrier Calcul Mental ${APPVERSION}"
+VOLICON="macos/icones/leterrier-calculment.icns"
 
 #On stoppe dès qu'on rencontre un problème
 set -e
 
-if [ -d /tmp/build-dmg ]; then
-  echo "Erreur: répertoire temporaire de build existant, merci de supprimer /tmp/build-dmg"
-  echo "et relancez ce script ensuite ..."
+if [ -d /tmp/build-dmg-${APPNAME} ]; then
+    rm -rf /tmp/build-dmg-${APPNAME}
 fi
+if [ -f ${APPNAME}-${APPVERSION}-osx.dmg ]; then
+    rm -f ${APPNAME}-${APPVERSION}-osx.dmg
+fi
+
 #on fabrique une arborescence "dmg" temporaire
-mkdir /tmp/build-dmg
+mkdir /tmp/build-dmg-${APPNAME}
 
 #on cherche ou on se trouve et on change de repertoire le cas echeant
 if [ -f ../${APPNAME}.pro ]; then
-  cd ..
+    cd ..
 fi
 
-#qmake
 qmake ${APPNAME}.pro -r -spec macx-g++ CONFIG+=release
 
 #compilation
@@ -29,12 +33,8 @@ make -w -j4
 #on utilise les outils de Qt pour trimbaler le Qt framework et les dependances Qt
 macdeployqt ${APPNAME}.app
 
-#copie des petites choses indispensables ...
-cp -a ${APPNAME}.app /tmp/build-dmg/
-cp -a MacOS/.DS_Store /tmp/build-dmg/
-cp -a MacOS/.background /tmp/build-dmg/
-#on n'a plus les liens symboliques, on doit donc les re-creer
-ln -s /Applications /tmp/build-dmg/Applications
+#note 20121021: je ne sais pas si c'est encore utile
+ln -s /Applications /tmp/build-dmg-${APPNAME}/Applications
 
 #copie des donnees
 cp -a data /tmp/build-dmg/${APPNAME}.app/Contents/MacOS/
@@ -42,10 +42,8 @@ cp -a conf /tmp/build-dmg/${APPNAME}.app/Contents/MacOS/
 cp -a lang /tmp/build-dmg/${APPNAME}.app/Contents/MacOS/
 
 #creation du fichier dmg
-hdiutil create ${APPNAME}-${APPVERSION}.dmg -srcfolder /tmp/build-dmg -format UDZO -volname ${APPNAME}
+~/create-dmg/create-dmg --window-size 415 295 --volname "${VOLNAME}" --volicon ${VOLICON} --background "macos/.background/background.png" ${APPNAME}-${APPVERSION}-osx.dmg ${APPNAME}.app
 
 #nettoyage
-rm -rf /tmp/build-dmg
-
-#echo "deplacement du fichier dmg sur le bureau ..."
-#mv *.dmg ~/Desktop
+rm -rf ${APPNAME}.app
+rm -rf /tmp/build-dmg-${APPNAME}
