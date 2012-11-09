939a6d32e664deeedf68b4b0b5d5c66d8d779e53
Eric Seigne <eric.seigne@ryxeo.com>
2012-11-09 11:41:58 +0100

    version.h ok pour windows

--- version.h
+++ version.h
@@ -1,10 +1,10 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             1,0,11
+#define VER_FILEVERSION             1,0,11,0
 #define VER_FILEVERSION_STR         "1.0.11\0"
 
-#define VER_PRODUCTVERSION          1,0,11
+#define VER_PRODUCTVERSION          1,0,11,0
 #define VER_PRODUCTVERSION_STR      "1.0.11\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
