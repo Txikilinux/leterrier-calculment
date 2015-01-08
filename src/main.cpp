c1acd7db625ac7e8e8985b88be072320d8681bcb
philippe.cadaugade <philippe.cadaugade@ryxeo.com>
2015-01-08 15:52:54 +0100

    Lancement direct par arguments sur la ligne de commande d'exercices customisés (quel exercice ? avec quelles options ?) - implémentation partielle de la demande #4049

--- src/main.cpp
+++ src/main.cpp
@@ -25,105 +25,138 @@
 
 #include "abuleduapplicationv1.h"
 #include "abuledusplashscreenv1.h"
+#include "abuleduetiquettesv1.h"
 #include "interface.h"
-#include "exercice.h"
+#include "exerciceoperation.h"
+#include "exercicemaisonnombres.h"
 #include "editeur.h"
 #include "version.h"
 #include <stdio.h>
 
-extern const QString abeApplicationLongName=QObject::trUtf8("AbulÉdu LeTerrier -- Calcul-Mental");
 
 int main(int argc, char *argv[])
 {
     AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, VER_COMPANYNAME_STR, VER_UNITVERSION_STR);
     a.setAbeApplicationLongName(QObject::trUtf8(VER_FILEDESCRIPTION_STR));
-    QList<AbulEduLogLevelV1> bannishedLogs;
-    bannishedLogs << WarnLevel << InfoLevel << TraceLevel;
-    a.setAbeApplicationBannishedLogLevel(bannishedLogs);
 
+    /** @todo regarder si c'est encore nécessaire, ça */
     abeApp->setProperty("langageUtilise",QLocale::system().name().section('_', 0, 0));
     abeApp->setProperty("utilisateur","");
     abeApp->setProperty("afficheBilanExercice",false);
-    QString exo;
-    QString nivo="";
-    int nombre=0;
-    /* Je teste si l'appel a des arguments, le premier étant forcément le nom de l'exercice */
-    if (argc>1) {
-        exo= argv[1];
-        exo.remove("--exercice=", Qt::CaseInsensitive);
-        exo.toLower();}
-//    qDebug()<<"Valeur exo : "<<exo;
 
-    /* Je teste si l'appel a d'autres arguments je récupère leur valeur, sachant qu'ici je me suis embêté à tester l'ordre des paramètres ce qui est inutile depuis la "normalisation" */
-    QString deuze;
-    QString troize;
-    if (argc>2){
-        deuze= argv[2];
-        troize=argv[3];
-//        qDebug()<<"deuze : "<<deuze<<"4ème lettre : "<<deuze[3];
-        if (deuze[3]=='i') {
-            deuze.remove("--niveau=", Qt::CaseInsensitive);
-            deuze.prepend("Niveau");
-            nivo=deuze;
-//            qDebug()<<"Exercice lancé directement avec niveau : "<<nivo;
-            }
-        else if (deuze[3]=='o') {
-            deuze.remove("--nombre=", Qt::CaseInsensitive);
-            nombre=deuze.toInt();
-//            qDebug()<<"Exercice lancé directement avec nombre : "<<nombre;
-            }
-        if (troize[3]=='i') {
-            troize.remove("--niveau=", Qt::CaseInsensitive);
-            troize.prepend("Niveau");
-            nivo=troize;
-//            qDebug()<<"Exercice lancé directement avec niveau : "<<nivo;
-            }
-        else if (troize[3]=='o') {
-            troize.remove("--nombre=", Qt::CaseInsensitive);
-            nombre=troize.toInt();
-//            qDebug()<<"Exercice lancé directement avec nombre : "<<nombre;
-            }
-        }
-#if defined(Q_OS_MAC)
-    QDir::setCurrent(abeApp->applicationDirPath());
-#endif
-    //à améliorer : dans un cas l'interface est créée pour rien, dans l'autre c'est l'exercice... de toute façon c'est de la mémoire utilisée pour rien !!
+    /** @todo corriger le commentaire ci-dessous */
+    /* On peut désormais lancer Aller avec des arguments
+     * par exemple, le lancement en console de "leterrier-aller --exercice=ttr --module=~/leterrier/abebox/leterrier-aller/Heureux_ulysse_ce1.abe --fond=~/leterrier/abebox/leterrier-aller/fondecran.jpg"
+     * lancera closure sur le module ulysse en utilisant comme fond d'écran le fichier fondecran.jpg  */
 
-
-    //Attention Usine à gaz : si on a un nombre j'ai besoin de l'ajouter ici pour avoir un truc du style tableM7 pour lire les valeurs dans le fichier de configuration
-    /*  -> pour lancement en ligne de commande
-    QString exo1 = exo;
-    if (nombre != 0) exo1 = exo+QString::number(nombre);
-    qDebug()<<"exo : "<<exo1;
-    //exercice e(exo,&a,nombre,nivo);
-
-    exercice e(exo1,0,nombre,nivo);
-//    qDebug()<<"Exercice lance avec comme parametres :";
-//    qDebug()<<"exo1 = "<<exo1;
-//    qDebug()<<"exo = "<<exo;
-//    qDebug()<<"nivo = "<<nivo;
-//    qDebug()<<"nombre = "<<nombre;
-//    qDebug()<<"temps accorde"<<e.m_temps;
-
-    //Attention Usine à gaz : mais je dois l'enlever ici pour appeler l'image qui s'appelle du style tableM(tout court).jpg
-        QPixmap* imageFond = new QPixmap(":/calculment/backgrounds/"+exo);
-        e.adapte(*imageFond);
-//        e.setImgFond(imageFond);
-//        e.setFixedSize(imageFond->width()+150,imageFond->height()+20);
-        //QObject* appliCastee = qobject_cast<QApplication*>(&appli);
-        e.setObjectName("exercoce");
-        //e.setParent(&w);//c'est ça qui mettait le bazar et créait la fenêtre blanche... je le commente, j'espère que ce n'était indispensable pour autre chose...
-        e.setWindowTitle("exercice");*/
-    if (argc==1)
+    QList<AbulEduLogLevelV1> bannishedLogs;
+    bannishedLogs << WarnLevel << InfoLevel << TraceLevel;
+    a.setAbeApplicationBannishedLogLevel(bannishedLogs);
+    QStringList arguments = a.arguments();
+    qDebug()<<arguments;
+    if (arguments.size() == 1)
     {
-        // ================== splashscreen
-    AbulEduSplashScreenV1 *splash = new AbulEduSplashScreenV1(0,true);
+        AbulEduSplashScreenV1 *splash = new AbulEduSplashScreenV1(0,true);
         splash->show();
         interface* w = new interface();
         splash->setMainWindow(w);
         splash->launch(1000);
     }
-//    else e.show(); -> pour lancement en ligne de commande
+    else {
+        QString exerciseType;
+//        QString unitName; --> le jour où on gèrera le travail en modules
+        QString background;
+        QString option = "-1";
+        QStringListIterator iter(arguments);
+        while(iter.hasNext()){
+            QString current = iter.next();
+            if(current.contains("--exercice")){
+                exerciseType = current.split("=").last();
+            }
+            /** @todo utiliser des modules -> version-3.0 (voir aussi les lignes commentées pour la QMessageBox::information et l'instanciation de l'AbulEduFileV1) */
+        /*  --> le jour où on gèrera le travail en modules
+            else if(current.contains("--module=")){
+                unitName = current.split("=").last();
+                unitName.replace("~",QDir::homePath());
+            } */
+            else if(current.contains("--fond=")){
+                background = current.split("=").last();
+                background.replace("~",QDir::homePath());
+            }
+            else if(current.contains("--option=")){
+                option = current.split("=").last();
+            }
+            else if(current.contains("--help")) {
+                QMessageBox::information(new QWidget(),"Aide","Vous pourrez lancer ce logiciel avec les options suivantes:\n\
+                                         \n\
+                                         --help -> cette aide\n\
+                                         \n\
+                                         --exercice=type exercice -> lance directement un exercice, choisissez parmi ExerciceClosure, ExerciceOrdreAlpha1, ExerciceMotsMelanges, ExercicePhrasesMelangees, ExerciceParagraphesMelanges, ExercicePhraseSansEspaces ou ExerciceRapido\n\
+//  --> le jour où on gèrera le travail en modules                                       \n\
+//  --> le jour où on gèrera le travail en modules                                        --module=nom module -> indiquez son chemin absolu\n\
+                        \n\
+                        --fond=image de fond -> indiquez son chemin absolu\n\
+                        ");
+                        return 0;
+            }
+        }
+        AbulEduEtiquettesV1* g = new AbulEduEtiquettesV1(QPointF(0,0));
+        g->setGeometry(QDesktopWidget().frameGeometry());
+        g->setAlignment(Qt::AlignLeft | Qt::AlignTop);
+        if(!background.isEmpty()){
+            QPixmap pixFond(background);
+            g->setBackgroundBrush(QBrush(pixFond.scaledToWidth(g->width())));
+        }
+        /*  --> le jour où on gèrera le travail en modules
+        AbulEduFileV1* abeFile = new AbulEduFileV1();
+        if(!abeFile->abeFileOpen(unitName)){
+            AbulEduMessageBoxV1* msg = new AbulEduMessageBoxV1(QObject::trUtf8("Problème !!"),QObject::trUtf8("Impossible d'ouvrir le module..."),true,g);
+            msg->show();
+            QObject::connect(msg, SIGNAL(signalAbeMessageBoxCloseOrHide()),g, SLOT(close()),Qt::UniqueConnection);
+        } */
+
+        if(QString::compare(exerciseType, "multiplication", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "division", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "addition", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "soustraction", Qt::CaseInsensitive) == 0){
+            ExerciceOperation *ex = new ExerciceOperation(exerciseType, g);
+            QObject::connect(ex, SIGNAL(signalExerciseExited()),g,SLOT(close()),Qt::UniqueConnection);
+        }
+        else if(QString::compare(exerciseType, "OdGrandeur", Qt::CaseInsensitive) == 0){
+            int number = 0;
+            if(QString::compare(option, "Addition", Qt::CaseInsensitive) != 0
+                    && QString::compare(option, "Soustraction", Qt::CaseInsensitive) != 0
+                    && QString::compare(option, "Multiplication", Qt::CaseInsensitive) != 0){
+                option = QString();
+                number = -1;
+            }
+            ExerciceOperation *ex = new ExerciceOperation(exerciseType.append(option), g,number);
+            QObject::connect(ex, SIGNAL(signalExerciseExited()),g,SLOT(close()),Qt::UniqueConnection);
+        }
+        else if(QString::compare(exerciseType, "tableM", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "tableA", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "complementA", Qt::CaseInsensitive) == 0
+                || QString::compare(exerciseType, "complementM", Qt::CaseInsensitive) == 0){
+            ExerciceOperation *ex = new ExerciceOperation(exerciseType, g,option.toInt());
+            ex->connect(ex, SIGNAL(signalExerciseExited()),g,SLOT(close()),Qt::UniqueConnection);
+        }
+        else if(QString::compare(exerciseType, "maisonDesNombres", Qt::CaseInsensitive) == 0){
+            int number = -1;
+            int optionInt = option.toInt();
+            if(optionInt == 0 || optionInt == 10){
+                number = optionInt;
+            }
+            ExerciceMaisonNombres* ex = new ExerciceMaisonNombres(exerciseType,g,number);
+            QObject::connect(ex, SIGNAL(signalExerciseExited()),g,SLOT(close()),Qt::UniqueConnection);
+        }
+        else {
+            /** @todo indiquer ici les valeurs possibles pour les noms d'exercice */
+            AbulEduMessageBoxV1* msg = new AbulEduMessageBoxV1(QObject::trUtf8("Problème !!"),QObject::trUtf8("Impossible de lancer l'exercice spécifié..."),true,g);
+            msg->show();
+            QObject::connect(msg, SIGNAL(signalAbeMessageBoxCloseOrHide()),g, SLOT(close()),Qt::UniqueConnection);
+        }
+//        qDebug()<<abeFile->abeFileGetContent(0).filePath(); --> le jour où on gèrera le travail en modules
+        g->show();
+    }
     return a.exec();
 }
-
